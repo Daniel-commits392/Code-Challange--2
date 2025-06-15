9f3f71cff8ba1b745488fe37ee9c0d2f3f5a8347
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-15 23:52:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const guests=['Glorian Wanja' ,'Hellen Wanjiru','Mallick Njagi','Victor Mugendi','Peter Mwangi','Grace Munge','Margrate Wambui']
+function addguests(name){
+if (guest.length>=10){
+    return "Error! Maximum number of guest is reached"
+}
+guests.push({ name:'allan',
+              rsvp: 'attending'
+}
+)
+}
+
+
